a33c0d2e3c75a3037a58730b105f1407db404ade
Leonardo Fontenele <128204050+leofontenele@users.noreply.github.com>
2023-11-28 06:36:47 +0000

    Update script2.js

--- javascript/script2.js
+++ javascript/script2.js
@@ -26,7 +26,7 @@ function Click2() {
  document.getElementById("frase1").innerHTML = "<br>Durante estes anos, trabalhei na cervejaria <b>100 Montaditos</b> para pagar a minha <br> faculdade de Relações Internacionais e o curso técnico de Web Development.<br> Além disso, fui voluntário do Núcleo de Apoio à Proteção Ambiental, no setor de <b>Marketing</b>";
 } 
 function Click3() { 
- document.getElementById("frase1").innerHTML = "<br>Atualmente, venho de um término de estágio como Software Engineer <b>(JavaScript, SQL e Outsystems)</b> pela <b>Lusodata - PDMFC</b> e, simultaneamente, <br>realizo alguns trabalhos como freelancer em plataformas comerciais <b>(Wordpress, Shopify, etc)</b> ou trabalhando em projetos específicos.";
+ document.getElementById("frase1").innerHTML = "<br>Atualmente, venho de um término de estágio como Software Engineer <b>(JavaScript, SQL e Outsystems)</b> pela <b>Lusodata - PDMFC</b>.<br>Após o termino do estágio, estou expandindo meu conhecimento na área e realizando alguns trabalhos como freelancer em plataformas comerciais <b>(Wordpress, Shopify, etc)</b> ou trabalhando em projetos específicos.";
 }
 function Unclick() { 
   document.getElementById("frase1").innerHTML = "Agora que você me conhece melhor, vamos conhecer <br> um pouco da minha carreira profissional? :) <br> Comecei a trabalhar aos 18 anos de idade e, hoje aos 24 anos, já percorri diversas áreas e estou em busca do meu sonho de progredir profissionalmente no ramo da tecnologia e crescer junto à alguma empresa! <br><br> <b>Deslize o mouse no selecionador ao lado e veja as minhas experiências!</b></p>";    
